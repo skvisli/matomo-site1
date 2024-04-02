64ade8a15b4023809618725206d971f0ff45e3d7
Sondre Kvisli <Sondre.Kvisli@skatteetaten.no>
2024-04-02 14:33:24 +0200

    Første commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <button>Klikk meg!</button>
+      <a
+        className="App-link"
+        href="https://skatteetaten.no"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Gå til skatteetaten.no
+      </a>
+    </div>
+  );
+}
+
+export default App;

ac225cf4d4d470e92e4b0528d5ef47aa2baf9bd6
Sondre Kvisli <Sondre.Kvisli@skatteetaten.no>
2024-04-03 09:24:05 +0200

    Add Matomo Tag Manager tracking code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
+interface MtmItem {
+  ["mtm.startTime"]: number;
+  event: string;
+}
+
+// Initialize _mtm as a global variable with the correct type
+declare global {
+  interface Window {
+    _mtm: MtmItem[];
+  }
+}
+
 function App() {
+  useEffect(() => {
+    const _mtm = (window._mtm = window._mtm || []);
+    _mtm.push({ "mtm.startTime": new Date().getTime(), event: "mtm.Start" });
+    (function () {
+      var d = document,
+        g = d.createElement("script"),
+        s = d.getElementsByTagName("script")[0];
+      g.async = true;
+      g.src =
+        "https://cdn.matomo.cloud/skvisli.matomo.cloud/container_hMpn3JxK.js";
+      s.parentNode?.insertBefore(g, s);
+    })();
+  }, []);
+
   return (
     <div className="App">
       <button>Klikk meg!</button>
